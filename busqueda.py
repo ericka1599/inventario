c12fd9ee50158368e9ef3e7830493c23fb8753bb
ericka1599 <erickasegura96@gmail.com>
2016-06-07 08:41:09 -0600

    Busqueda de archivo

--- busqueda.py
+++ busqueda.py
@@ -1,6 +1,7 @@
 r = int(input("Desea buscar un producto? si = 1 no = 2"))
-if r == 1 
+if r == 1 :
 	archivo = open("productos.txt" "rb")
 	pro = input("Ingrese el nombre del producto que desea buscar: ")
-	for line in "productos.txt"
+	listaB = pickle.load (archivo)
+	pro.lower ()
 	
